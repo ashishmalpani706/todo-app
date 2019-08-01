69ed450b92e35451cf9044cf81bf34b5e2934405
Ashish Malpani <ashishmalpani706@gmail,com>
2019-08-01 11:11:56 -0400

    Cleaned un-necessary Counter Button Functions

--- frontend/todo-app/src/components/counter/Counter.jsx
+++ frontend/todo-app/src/components/counter/Counter.jsx
@@ -21,7 +21,7 @@ class Counter extends Component {
             <CounterButton by={5} incrementMethod={this.increment} decrementMethod={this.decrement}/>
             <CounterButton by={10} incrementMethod={this.increment} decrementMethod={this.decrement}/>
             <span className="count">{this.state.counter}</span>
-            <div><button className="reset" onClick={this.reset}>RESET</button></div>
+            <div><button className="reset"  onClick={this.reset}>RESET</button></div>
         </div>
         );
     }
@@ -52,36 +52,13 @@ class Counter extends Component {
 
 class CounterButton extends Component {
     
-    constructor() {
-        super();
-        this.state = {
-            counter : 0
-        }
-        this.increment = this.increment.bind(this);
-        this.decrement = this.decrement.bind(this);
-    }
-
     render() {
         return (
         <div className="counterButton">
-            <button onClick={this.increment}>+{this.props.by}</button>
-            <button onClick={this.decrement}>-{this.props.by}</button>
+            <button onClick={() => this.props.incrementMethod(this.props.by)}>+{this.props.by}</button>
+            <button onClick={() => this.props.decrementMethod(this.props.by)}>-{this.props.by}</button>
         </div>
         );
-    }
-
-    increment() {
-        this.setState({
-             counter : this.state.counter + this.props.by
-        });
-        this.props.incrementMethod(this.props.by)
-    }
-
-    decrement(by) {
-        this.setState({
-            counter : this.state.counter - this.props.by
-       });
-       this.props.decrementMethod(this.props.by)
     }
 
 }
