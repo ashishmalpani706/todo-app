acf354758e43d16f434e59fa304d5a42f886b630
Ashish Malpani <ashishmalpani706@gmail,com>
2019-08-01 14:28:44 -0400

    Login works

--- frontend/todo-app/src/components/todo/ToDoApp.jsx
+++ frontend/todo-app/src/components/todo/ToDoApp.jsx
@@ -17,7 +17,9 @@ class LoginComponent extends Component {
 
         this.state = {
             username : '',
-            password : ''
+            password : '',
+            hasLoginFailed : false,
+            showSuccess: false
         }
 
         this.handleChange = this.handleChange.bind(this);
@@ -27,6 +29,8 @@ class LoginComponent extends Component {
     render() {
         return (
             <div>
+                <ShowSuccess showSuccess={this.state.showSuccess}/>
+                <ShowInvalidCredentials hasLoginFailed={this.state.hasLoginFailed}/>
                 Username : <input type="text" name="username" value={this.state.username} onChange={this.handleChange}/>
                 Password : <input type="password" name="password" value={this.state.password} onChange={this.handleChange}/>
                 <button onClick={this.loginClicked}>Submit</button>
@@ -42,11 +46,29 @@ class LoginComponent extends Component {
     loginClicked(event) {
         if(this.state.username === 'sample' && this.state.password === 'password') {
             console.log('Approved');
+            this.setState({showSuccess : true});
+            this.setState({hasLoginFailed : false});
         }
         else {
             console.log('Failed');
+            this.setState({hasLoginFailed : true});
+            this.setState({showSuccess : false});
         }
     }
+}
+
+function ShowInvalidCredentials(props) {
+    if(props.hasLoginFailed) {
+        return <div>Invalid Credentails</div>
+    }
+    return null;
+}
+
+function ShowSuccess(props) {
+    if(props.showSuccess) {
+        return <div>Successful Login</div>
+    }
+    return null;
 }
 
 export default ToDoApp;
